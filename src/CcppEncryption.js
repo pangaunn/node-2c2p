5f1612d536ab9800cb8c2a1342d3248326ee51ea
aunn <aunn_it08@hotmail.com>
2018-05-25 23:17:36 +0700

    add encryptWithCert function

--- src/CcppEncryption.js
+++ src/CcppEncryption.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const forge = require('node-forge')
 
 export const CcppEncryption = {
   encrypt (xmlString, keyPath) {
@@ -6,6 +7,17 @@ export const CcppEncryption = {
     return this.encryptWithCert(xmlString, cert)
   },
   encryptWithCert (xmlString, cert) {
+    const certFormPem = forge.pki.certificateFromPem(cert)
 
+    let p7 = forge.pkcs7.createEnvelopedData()
+    p7.addRecipient(certFormPem)
+    p7.content = forge.util.createBuffer()
+    p7.content.putString(xmlString)
+    p7.encrypt()
+
+    const pem = forge.pkcs7.messageToPem(p7)
+    let returnPEM = pem.substring(23).trim()
+    returnPEM = returnPEM.replace('-----END PKCS7-----', '')
+    return returnPEM.replace(/\r?\n|\r/g, '')
   }
 }
